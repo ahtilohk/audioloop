935e17f74b00c8b060d3068e58ad7297ac5ac5e9
Ahti <ahti.lohk@taltech.ee>
2026-01-15 20:35:50 +0200

    Add missing Share icon to AppIcons.kt

--- app/src/main/java/com/example/audioloop/AppIcons.kt
+++ app/src/main/java/com/example/audioloop/AppIcons.kt
@@ -553,4 +553,38 @@ object AppIcons {
             return _delete!!
         }
     private var _delete: ImageVector? = null
+
+    // Replacement for Icons.Default.Share
+    val Share: ImageVector
+        get() {
+            if (_share != null) return _share!!
+            _share = materialIcon(name = "Share") {
+                materialPath {
+                    moveTo(18.0f, 16.08f)
+                    curveToRelative(-0.76f, 0.0f, -1.44f, 0.3f, -1.96f, 0.77f)
+                    lineTo(8.91f, 12.7f)
+                    curveToRelative(0.05f, -0.23f, 0.09f, -0.46f, 0.09f, -0.7f)
+                    reflectiveCurveToRelative(-0.04f, -0.47f, -0.09f, -0.7f)
+                    lineToRelative(7.05f, -4.11f)
+                    curveToRelative(0.53f, 0.47f, 1.21f, 0.77f, 1.96f, 0.77f)
+                    curveToRelative(1.66f, 0.0f, 3.0f, -1.34f, 3.0f, -3.0f)
+                    reflectiveCurveToRelative(-1.34f, -3.0f, -3.0f, -3.0f)
+                    reflectiveCurveToRelative(-3.0f, 1.34f, -3.0f, 3.0f)
+                    curveToRelative(0.0f, 0.24f, 0.04f, 0.47f, 0.09f, 0.7f)
+                    lineTo(8.04f, 9.81f)
+                    curveTo(7.5f, 9.31f, 6.79f, 9.0f, 6.0f, 9.0f)
+                    curveToRelative(-1.66f, 0.0f, -3.0f, 1.34f, -3.0f, 3.0f)
+                    reflectiveCurveToRelative(1.34f, 3.0f, 3.0f, 3.0f)
+                    curveToRelative(0.79f, 0.0f, 1.5f, -0.31f, 2.04f, -0.81f)
+                    lineToRelative(7.12f, 4.16f)
+                    curveToRelative(-0.05f, 0.21f, -0.08f, 0.43f, -0.08f, 0.65f)
+                    curveToRelative(0.0f, 1.61f, 1.31f, 2.92f, 2.92f, 2.92f)
+                    reflectiveCurveToRelative(2.92f, -1.31f, 2.92f, -2.92f)
+                    reflectiveCurveToRelative(-1.31f, -2.92f, -2.92f, -2.92f)
+                    close()
+                }
+            }
+            return _share!!
+        }
+    private var _share: ImageVector? = null
 }
