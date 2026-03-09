062f73330881de1b887098c445291afd74b571ba
Ahti <ahti.lohk@taltech.ee>
2026-03-09 21:11:59 +0200

    Add Folder icon to AppIcons and fix remaining compilation error

--- app/src/main/java/com/example/audioloop/AppIcons.kt
+++ app/src/main/java/com/example/audioloop/AppIcons.kt
@@ -1342,6 +1342,29 @@ object AppIcons {
         }
     private var _save: ImageVector? = null
 
+    val Folder: ImageVector
+        get() {
+            if (_folder != null) return _folder!!
+            _folder = materialIcon(name = "Folder") {
+                materialPath {
+                    moveTo(10.0f, 4.0f)
+                    horizontalLineTo(4.0f)
+                    curveToRelative(-1.1f, 0.0f, -1.99f, 0.9f, -1.99f, 2.0f)
+                    lineTo(2.0f, 18.0f)
+                    curveToRelative(0.0f, 1.1f, 0.9f, 2.0f, 2.0f, 2.0f)
+                    horizontalLineToRelative(16.0f)
+                    curveToRelative(1.1f, 0.0f, 2.0f, -0.9f, 2.0f, -2.0f)
+                    verticalLineTo(8.0f)
+                    curveToRelative(0.0f, -1.1f, -0.9f, -2.0f, -2.0f, -2.0f)
+                    horizontalLineToRelative(-8.0f)
+                    lineToRelative(-2.0f, -2.0f)
+                    close()
+                }
+            }
+            return _folder!!
+        }
+    private var _folder: ImageVector? = null
+
     val Done: ImageVector
         get() = Check
 
