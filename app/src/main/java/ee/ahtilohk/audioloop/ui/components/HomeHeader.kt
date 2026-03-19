ac88dabb4eb4af64e472b9fda4f5b742511f9883
Ahti <ahti.lohk@taltech.ee>
2026-03-19 16:24:47 +0200

    fix: add missing FontFamily import in HomeHeader.kt

--- app/src/main/java/ee/ahtilohk/audioloop/ui/components/HomeHeader.kt
+++ app/src/main/java/ee/ahtilohk/audioloop/ui/components/HomeHeader.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
